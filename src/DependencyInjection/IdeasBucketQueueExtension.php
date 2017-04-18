327c10958955e5a85f9958015644a76c3b34920f
Danny van Wijk <info@vwmedia.nl>
2017-04-18 18:57:33 +0000

    Set alias in services.xml

--- src/DependencyInjection/IdeasBucketQueueExtension.php
+++ src/DependencyInjection/IdeasBucketQueueExtension.php
@@ -44,8 +44,6 @@ class IdeasBucketQueueExtension extends Extension
             $container->setAlias('ideasbucket_queue.redis_client', $mergedConfiguration['connections']['redis']['client']);
         }
 
-        $container->setAlias('idb_queue', 'ideasbucket_queue');
-
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
         $loader->load('connectors.xml');
